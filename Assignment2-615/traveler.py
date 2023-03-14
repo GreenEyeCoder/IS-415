aa36549aa63177681de09e5f012748fcfb2a26ac
Joel DeRouchey <joel.derouchey@gmail.com>
2023-03-14 02:50:42 -0700

    thinking this is good

--- Assignment2-615/traveler.py
+++ Assignment2-615/traveler.py
@@ -1,100 +1,113 @@
 import csv
-import os.path
+import os
+import random
+
+
 
 # define file paths for road, health, and inventory
 road_file = "road.csv"
-health_file = "health.txt"
-inventory_file = "inventory.txt"
+health_file = "health"
+inventory_file = "inventory"
 
 # initialize player's health and inventory
-player_health = 100
+''' Global variables '''
+double_movement = False
 potion_available = True
+player_health = 100
 
-# define functions to retrieve current position, current health, and current inventory
+
+# define function to retrieve current position
 def get_current_position():
     try:
-        with open(road_file, "a+") as f:
-            reader = csv.reader(f)
-        for row in reader:
-            if "X" in row:
-                return row.index("X") + 1
-    except:
-        print("Error with current position function")
+        if os.path.isfile(road_file) == False:
+            with open(road_file, "w") as f:
+                f.write("X,,,,,,,,,")
+                return 1
+        else:
+            with open(road_file, "r") as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if "X" in row:
+                        return (row.index("X") + 1)
+                return 1  # Return a default value if X is not found
+    except FileNotFoundError:
+        print("The file does not exist.")
+        return 1  # Return a default value on error
+    except PermissionError:
+        print("You don't have permission to access the file.")
+        return 1  # Return a default value on error
+    except Exception as e:
+        print("An error occurred:", e)
+        return 1  # Return a default value on error
+
+
+def update_position(steps):
+    new_position = get_current_position() + steps
+    with open(road_file, 'w') as f:
+        for steps in range(10):
+            if steps +1  == new_position:
+                f.write('X')
+            else:
+                f.write(',')
+    return 0
+        
 
 def get_current_health():
     try:
         if os.path.isfile(health_file) == False:
-            with open(health_file, "a+") as f:
-                f.write(100)
-                return int(f.read())
+            with open(health_file, 'w') as f:
+                f.write(str(100))
+                return 100
         else:
-            with open(health_file, "a+") as f:
+            with open(health_file, 'r') as f:
                 return int(f.read())
-    except:
-        print("Error with current health function")
+    except FileNotFoundError:
+        print("The file does not exist.")
+    except PermissionError:
+        print("You don't have permission to access the file.")
+    except Exception as e:
+        print("An error occurred:", e)
+
+
+def update_health(health):
+    with open(health_file, 'w') as f:
+        f.write(str(health))  
 
 def get_inventory():
     try:
         if os.path.isfile(inventory_file) == False:
-            with open(inventory_file, "a+") as f:
-                f.write(1)
-                return int(f.read())
+            with open(inventory_file, 'w') as f:
+                f.write(str(70))
+                return 70
         else:
-        with open(inventory_file, "a+") as f:
-            return int(f.read())
-    except:
-        print("Error with get_inventory function")
+            with open(inventory_file, 'r') as f:
+                return int(f.read())
+    except FileNotFoundError:
+        print("The file does not exist.")
+    except PermissionError:
+        print("You don't have permission to access the file.")
+    except Exception as e:
+        print("An error occurred:", e)
 
 # define functions for each possible action
+
 def roll_dice():
-    # define dice outcomes and their effects
-    dice_outcomes = {
-        1: -10,
-        2: -1,
-        3: "pray",
-        4: -40,
-        5: "double",
-        6: 1
-    }
-    # roll the dice and determine the outcome
     import random
-    outcome = random.randint(1, 6)
-    if dice_outcomes[outcome] == "pray":
-        prayer_outcome = random.randint(1, 10)
-        if prayer_outcome == 7:
-            return "win"
-        else:
-            return "no effect"
-    elif dice_outcomes[outcome] == "double":
-        # set a flag to indicate that the player's next movement will be doubled
-        global double_movement
-        double_movement = True
-        return "no effect"
-    else:
-        # update the player's health and position
-        global player_health
-        if double_movement:
-            double_movement = False
-            outcome *= 2
-        player_health += dice_outcomes[outcome]
-        current_position = get_current_position()
-        new_position = current_position + outcome
-        if new_position < 1:
-            new_position = 1
-        elif new_position > 10:
-            new_position = 10
-        update_road(current_position, new_position)
-        return outcome
+    die = random.randint(1, 6)
+    return die
+
 
 def consume_potion():
-    global player_health
     global potion_available
+    global player_health
+
     if potion_available:
         player_health += 70
         if player_health > 100:
             player_health = 100
         potion_available = False
-        update_inventory(potion_available)
+        update_health(player_health)
+        update_inventory(potion_available )
         return "potion used"
     else:
         return "no potion"
@@ -109,19 +122,8 @@ def quit_game():
         print("You lose.")
 
 # define functions to update the road, health, and inventory files
-def update_road(old_position, new_position):
-    with open(road_file, "r") as f:
-        reader = csv.reader(f)
-        rows = [row for row in reader]
-        rows[0][old_position - 1] = "-"
-        rows[0][new_position - 1] = "X"
-    with open(road_file, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(rows)
 
-def update_health(current_health):
-    with open(health_file, "w") as f:
-        f.write(str(current_health))
+
 
 def update_inventory(potion_available):
         with open(inventory_file, "w") as f:
@@ -129,29 +131,92 @@ def update_inventory(potion_available):
                 f.write("1")
             else:
                 f.write("0")
-'''define a function to display the current status to the user'''
-def display_status():
-        current_position = get_current_position()
-        current_health = get_current_health()
-        current_inventory = get_inventory()
-        print("Current position:", current_position)
-        print("Current health:", current_health)
-        print("Potion available:", "Yes" if current_inventory else "No")
+
+
+'''Create Road Map'''
+def roadMap(position):
+    global double_movement
+    gameMap = []
+    for steps in range(10):
+        if steps == position - 1:
+            if double_movement == True:
+                gameMap.append('XX')
+            else:
+                gameMap.append('X')
+        else:
+            gameMap.append('|')
+    return(gameMap)
+
+
+def dice_outcomes(die):
+        outcomes = {
+        1: -10,
+        2: -1,
+        3: "pray",
+        4: -40,
+        5: "double",
+        6: 1
+    }
+        return outcomes[die]
+
+def getCurrentHealthStatus():
+    global player_health
+    player_health = get_current_health()
+    print(f'Your current health is :{player_health}')
+
+def gameStatus():
+    getCurrentHealthStatus()
+    global double_movement
+    position = get_current_position() 
+    myMap = roadMap(position)
+    print(*myMap,sep=' ')
 
 '''define the main game loop'''
 def game_loop():
-    while True:
-        display_status()
-        user_input = input("Enter r to roll the dice, p to consume a potion, or q to quit: ")
+    global double_movement
+    global player_health
+    '''Starting game message'''
+    
+
+    gameStatus()
+    print("Your journey begins.  you are")
+    print("only at the first position.\n\n")
+    
+    while get_current_health() > 0:
+        user_input = input("What will you do, Traveler?\n")
         if user_input == "r":
-            outcome = roll_dice()
-            print("You rolled a", outcome)
+            die = roll_dice()
+            print("You rolled a", die)
+            outcome = dice_outcomes(die)
             if outcome == "win":
                 print("You win!")
                 break
-            elif outcome == "no effect":
-                print("Nothing happened.")
+            elif outcome == -10 or outcome == -40:
+                player_health = get_current_health() + outcome 
+                update_health(player_health)
+                print(f'You took {abs(outcome)} damage')
+                                
+            elif outcome == 1 or outcome == -1:
+                update_position(outcome)    # roll the dice and determine the outcome
+                if double_movement == True:
+                    update_position(outcome)
+                    double_movement = False
+                if outcome == 1:
+                    print("You move forward")
+                else:
+                    print("You move back")
+            elif outcome == "pray":
+                prayer_outcome = random.randint(1, 10)
+                if prayer_outcome == 7:
+                    print ("You win!")
+                    break
+                else:
+                    print("Nobody heard myour prayers")
+            elif outcome == "double":
+                double_movement = True
+                # write to file the double XX          
         elif user_input == "p":
+            potion_available = 70 - player_health
             outcome = consume_potion()
             if outcome == "potion used":
                 print("You consumed a potion and restored some health.")
@@ -162,9 +227,23 @@ def game_loop():
             break
         else:
             print("Invalid input. Please try again.")
-            
+        
+        gameStatus()
 
     '''start the game'''
-double_movement = False
+
 game_loop()
+
+'''Game clean up'''
+try:
+    os.remove(road_file)
+    os.remove(health_file)
+    os.remove(inventory_file)
+
+except FileNotFoundError:
+    print("The file does not exist.")
+except PermissionError:
+    print("You don't have permission to access the file.")
+except Exception as e:
+    print("An error occurred:", e)
 
